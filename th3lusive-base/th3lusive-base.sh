83fc0fe12bf11e27e7ab226dc1cc606beab5eb49
th3lusive <brandond44@gmail.com>
2018-12-22 00:20:15 +0800

    Added Personal meta-package

---
+++ th3lusive-base/th3lusive-base.sh
@@ -0,0 +1,9 @@
+export EDITOR='vim'
+export PATH="${PATH}:$HOME/.config/scripts"
+export PATH="${PATH}:$HOME/.local/bin"
+export PATH="$HOME/.node_modules/bin:$PATH"
+export npm_config_prefix=~/.node_modules
+export GEM_HOME=$HOME/.gem
+
+alias ls=/usr/bin/exa
+alias cat=/usr/bin/bat
